41ed161a611d98c66c84d4c7a93bfd64dea98c49
Махонин Игорь <Imakhonin24ru@gmail.com>
2019-12-08 16:10:34 +0700

    Увеличил параметры в globalParams

--- src/main/java/strategy/ParamsBuilder.java
+++ src/main/java/strategy/ParamsBuilder.java
@@ -15,22 +15,74 @@ public class ParamsBuilder {
 	protected EnemyController enemyController;
 	protected LootBoxController lootBoxController;
 	
+	private double velocity;
+	private boolean jump;
+	private boolean jumpDown;
+	private Vec2Double aim;
+	private boolean shoot;
+	private boolean reload;
+	private boolean swapWeapon;
+    private boolean plantMine;	
+    
 	public ParamsBuilder(Unit unit, Game game) {
 		this.unit = unit;
 		this.game = game;
 	}
 
+	//---------------Setter block----------------
+	
 	public ParamsBuilder setEnemyController(EnemyController enemyController) {
 		this.enemyController = enemyController;
 		return this;
 	}
-
+	
 	public ParamsBuilder setLootBoxController(LootBoxController lootBoxController) {
 		this.lootBoxController = lootBoxController;
 		return this;
 	}
 	
+	public ParamsBuilder setJump(boolean jump) { 
+    	this.jump = jump;
+    	return this;
+    }
 	
+	public ParamsBuilder setVelocity(double velocity) { 
+		this.velocity = velocity;
+		return this;
+	}
+    
+	public ParamsBuilder setAim(model.Vec2Double aim) { 
+		this.aim = aim; 
+		return this;
+	}
+    
+	public ParamsBuilder setShoot(boolean shoot) { 
+		this.shoot = shoot;
+		return this;
+	}
+    
+	public ParamsBuilder setReload(boolean reload) { 
+		this.reload = reload;
+		return this;
+	}
+    
+	public ParamsBuilder setSwapWeapon(boolean swapWeapon) { 
+		this.swapWeapon = swapWeapon;
+		return this;
+	}
+    
+	public ParamsBuilder setPlantMine(boolean plantMine) { 
+		this.plantMine = plantMine;
+		return this;
+	}
+	
+	public ParamsBuilder setJumpDown(boolean jumpDown) { 
+		this.jumpDown = jumpDown;
+		return this;
+	}
+    
+	
+	//---------------Getter block----------------
 	
 	public Unit getUnit() {
 		return unit;
@@ -48,6 +100,36 @@ public class ParamsBuilder {
 		return lootBoxController;
 	}
 	
+	public double getVelocity() { 
+		return velocity; 
+	}
 	
-	
+    public boolean hetJump() { 
+    	return jump;
+	}
+    
+    public boolean getJumpDown() { 
+    	return jumpDown;
+	}
+
+    public Vec2Double getAim() { 
+    	return aim;
+    }
+    
+    public boolean getShoot() { 
+    	return shoot;
+    }
+    
+    public boolean getReload() {
+    	return reload;
+    }
+    
+    public boolean getSwapWeapon() {
+    	return swapWeapon;
+    }
+    
+    public boolean getPlantMine() {
+    	return plantMine;
+    }
+    
 }
